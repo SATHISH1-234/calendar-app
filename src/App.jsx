cdde56da1ab5fcce28dcbfd79060502b4f4a815a
SATHISH1-234 <sathishprofessional2004@gmail.com>
2025-06-25 18:35:46 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Calendar } from "./components/calendar";
+import { Calendar } from "./components/Calendar";
 import { EventModal } from "./components/Eventmodal";
 
 const staticEvents = [
